7fe6afcea2a5ec6e5f2386cc4c051499e410750d
IagoNicolas <iago.nicolas@ufpe.br>
2023-12-17 21:03:00 -0300

    Library updates

--- app/src/main/java/com/example/poke_wear_app/presentation/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/example/poke_wear_app/presentation/viewmodel/PokemonViewModel.kt
@@ -1,14 +1,9 @@
 package com.example.poke_wear_app.presentation.viewmodel
 
-import android.content.Context
-import android.util.Log
-import android.widget.Toast
-import androidx.compose.ui.geometry.Offset
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.poke_wear_app.presentation.MainActivity
 import com.example.poke_wear_app.presentation.api.model.PokemonList
 import com.example.poke_wear_app.presentation.api.repository.PokemonRepository
 import com.example.poke_wear_app.presentation.api.repository.ResultWrapper
