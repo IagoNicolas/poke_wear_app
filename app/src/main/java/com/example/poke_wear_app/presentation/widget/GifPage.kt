899a79f609ee578c7dc66635cc3c225b18c92992
IagoNicolas <iago.nicolas@ufpe.br>
2023-12-25 01:45:09 -0300

    Added shiny button to GifPage.kt and icon to StatsPage.kt

--- app/src/main/java/com/example/poke_wear_app/presentation/widget/GifPage.kt
+++ app/src/main/java/com/example/poke_wear_app/presentation/widget/GifPage.kt
@@ -3,28 +3,45 @@ package com.example.poke_wear_app.presentation.widget
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.ToggleButton
 import coil.ImageLoader
 import coil.compose.AsyncImage
 import coil.decode.ImageDecoderDecoder
+import com.example.poke_wear_app.R
 
 @Composable
 fun GifPage(index: Int) {
+    var checked by remember { mutableStateOf(false) }
+    var link by remember {
+        mutableStateOf(
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${
+                index.plus(
+                    1
+                )
+            }.gif"
+        )
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 36.dp),
     ) {
         AsyncImage(
-            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${
-                index.plus(
-                    1
-                )
-            }.gif",
+            model = link,
             modifier = Modifier
                 .fillMaxSize()
                 .scale(0.9f),
@@ -33,5 +50,33 @@ fun GifPage(index: Int) {
                 add(ImageDecoderDecoder.Factory())
             }.build()
         )
+
+        ToggleButton(modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 8.dp)
+            .size(24.dp),
+            checked = checked,
+            onCheckedChange = {
+                checked = it
+                if (!checked) {
+                    link = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${
+                        index.plus(
+                            1
+                        )
+                    }.gif"
+                } else {
+                    link = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${
+                        index.plus(
+                            1
+                        )
+                    }.gif"
+                }
+            }) {
+            Icon(
+                modifier = Modifier.padding(all = 4.dp),
+                painter = painterResource(id = R.drawable.shiny_24),
+                contentDescription = null
+            )
+        }
     }
 }
--- app/src/main/java/com/example/poke_wear_app/presentation/widget/StatsPage.kt
+++ app/src/main/java/com/example/poke_wear_app/presentation/widget/StatsPage.kt
@@ -1,13 +1,19 @@
 package com.example.poke_wear_app.presentation.widget
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.poke_wear_app.R
 import com.example.poke_wear_app.presentation.api.model.Pokemon
 
@@ -15,42 +21,55 @@ import com.example.poke_wear_app.presentation.api.model.Pokemon
 fun StatsPage(result: Pokemon) {
     val scrollState = rememberScrollState()
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(state = scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize(),
     ) {
-        AttributeSlider(
-            result.stats[0].baseStat.toFloat() / 51,
-            R.drawable.hp_24,
-            2
-        )
-        AttributeSlider(
-            result.stats[1].baseStat.toFloat() / 36,
-            R.drawable.attack_24,
-            2
-        )
-        AttributeSlider(
-            result.stats[2].baseStat.toFloat() / 40,
-            R.drawable.shield_24,
-            2
-        )
-        AttributeSlider(
-            result.stats[3].baseStat.toFloat() / 36,
-            R.drawable.sp_attack_24,
-            2
-        )
-        AttributeSlider(
-            result.stats[4].baseStat.toFloat() / 40,
-            R.drawable.sp_shield_24,
-            2
-        )
-        AttributeSlider(
-            result.stats[5].baseStat.toFloat() / 40,
-            R.drawable.speed_24,
-            0
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            AttributeSlider(
+                result.stats[0].baseStat.toFloat() / 51,
+                R.drawable.hp_24,
+                2
+            )
+            AttributeSlider(
+                result.stats[1].baseStat.toFloat() / 36,
+                R.drawable.attack_24,
+                2
+            )
+            AttributeSlider(
+                result.stats[2].baseStat.toFloat() / 40,
+                R.drawable.shield_24,
+                2
+            )
+            AttributeSlider(
+                result.stats[3].baseStat.toFloat() / 36,
+                R.drawable.sp_attack_24,
+                2
+            )
+            AttributeSlider(
+                result.stats[4].baseStat.toFloat() / 40,
+                R.drawable.sp_shield_24,
+                2
+            )
+            AttributeSlider(
+                result.stats[5].baseStat.toFloat() / 40,
+                R.drawable.speed_24,
+                0
+            )
+        }
+        AsyncImage(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.BottomCenter),
+            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${
+                result.id
+            }.png", contentDescription = null
         )
     }
 }
