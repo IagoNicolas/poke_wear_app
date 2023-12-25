2e36b589f8aa745fbdef187fbac8cbba0cb9dfad
IagoNicolas <iago.nicolas@ufpe.br>
2023-12-25 01:45:21 -0300

    Added shiny button to GifPage.kt and icon to StatsPage.kt

--- app/src/main/java/com/example/poke_wear_app/presentation/widget/StatsPage.kt
+++ app/src/main/java/com/example/poke_wear_app/presentation/widget/StatsPage.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.poke_wear_app.R
